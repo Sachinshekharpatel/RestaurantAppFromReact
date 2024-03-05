e82488b059096ad8a8c4874b65a4b356a3d12166
Sachinshekharpatel <heroft7024@gmail.com>
2024-03-05 12:12:38 +0530

    Header section only

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from "./Component/header";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
